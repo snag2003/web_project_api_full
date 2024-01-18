829f6a3c1084b6932ee2cd50ab4fbf98dad10dff
Stephany Acosta <stephanynag@gmail.com>
2024-01-17 22:58:27 -0500

    updating site

--- frontend/src/utils/auth.js
+++ frontend/src/utils/auth.js
@@ -1,4 +1,4 @@
-export const BASE_URL = "https://api.stephanydev.justlearning.net";
+export const BASE_URL = "https://api.stephany.developer.li";
 
 export const register = (email, password) => {
   return fetch(`${BASE_URL}/signup`, {
